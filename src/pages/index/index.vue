7291f35038fa1e0099367e43e6c027d5a11965fa
剑来 <352532060@qq.com>
2018-08-27 18:22:00 +0800

    提纲

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -66,32 +66,13 @@ export default {
 </script>
 
 <style scoped>
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
-}
-
-.userinfo-nickname {
-  color: #aaa;
-}
-
-.usermotto {
-  margin-top: 150px;
-}
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
-}
-
+  .container {
+    padding: 44rpx 0;
+    box-sizing: border-box;
+    width:100%;
+    height:100%;
+    min-height: 100vh;
+    background:url("https://www.remapcity.com/fileserver/static/mini/yori/night.png") no-repeat;
+    background-size:cover;
+  }
 </style>
